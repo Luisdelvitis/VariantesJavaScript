464f14f44e787ee2145e0833449234a960886840
LSF Luis Silva <luisfernandodepaulasouza@outlook.com>
2023-10-24 22:10:38 -0300

    VariablesJavaScript

---
+++ Variables.js
@@ -0,0 +1,27 @@
+//String                                   Representa datos textuales, cadenas de caracteres
+
+var bootcamp = "Henry";
+
+//Numeros //Number                         Permite representar y manipular valores -37 37 3.7 (...)
+
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.75;
+
+//Boleanos //Boolean                       Representa un valor lógico y puede tener dos valores ya sean true or false
+                                           
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined                                Representa una var que no ha sido declara o no tiene valor
+
+var cajon1;
+
+console.log(cajon1);
+
+//Null                                     Ausencia de valor
+
+var sinNada = null;
